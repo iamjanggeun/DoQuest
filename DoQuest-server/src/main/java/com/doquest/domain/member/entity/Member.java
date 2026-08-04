16e93e6f2da225949350257311ab8af24e6d02ae
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-08-04 21:51:37 +0900

    feat:Member Entity 추가

--- DoQuest-server/src/main/java/com/doquest/domain/member/entity/Member.java
+++ DoQuest-server/src/main/java/com/doquest/domain/member/entity/Member.java
@@ -1,4 +1,54 @@
 package com.doquest.domain.member.entity;
 
-public class Member {
+import com.doquest.domain.pet.entity.Pet;
+import com.doquest.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(
+        name = "members",
+        indexes = {
+                @Index(name = "idx_member_email", columnList = "email", unique = true)
+        }
+)
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자 접근 제어
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, length = 30)
+    private String nickname;
+    @Enumerated(EnumType.STRING) // Enum을 DB에 저장 시 STRING으로 저장 (ORDINAL X)
+    @Column(nullable = false, length = 20)
+    private Role role;
+
+    // Member가 연관관계의 주인 (pet_id FK 보유)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "pet_id", foreignKey = @ForeignKey(name = "fk_members_to_pets"))
+    private Pet pet;
+
+    // == Factory Method == //
+    public static Member createMember(String email, String password, String nickname, Pet pet) {
+        Member member = new Member();
+        member.email = email;
+        member.password = password;
+        member.nickname = nickname;
+        member.role = Role.USER;
+        member.pet = pet;
+        return member;
+    }
 }
--- DoQuest-server/src/main/java/com/doquest/domain/member/entity/Role.java
+++ DoQuest-server/src/main/java/com/doquest/domain/member/entity/Role.java
@@ -1,4 +1,15 @@
 package com.doquest.domain.member.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
+
+    USER("ROLE_USER", "일반 사용자"),
+    ADMIN("ROLE_ADMIN", "시스템 관리자");
+
+    private final String key;
+    private final String title;
 }
