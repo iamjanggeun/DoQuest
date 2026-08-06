cad26d93f02b274b061a63f916f7ec5016a3a944
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-08-07 00:59:57 +0900

    fix:상속 extends 안했던거 수정

--- DoQuest-server/src/main/java/com/doquest/domain/quest/repository/QuestRepository.java
+++ DoQuest-server/src/main/java/com/doquest/domain/quest/repository/QuestRepository.java
@@ -1,9 +1,10 @@
 package com.doquest.domain.quest.repository;
 
 import com.doquest.domain.quest.entity.Quest;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface QuestRepository {
+public interface QuestRepository extends JpaRepository<Quest, Long> {
     List<Quest> findByMemberIdAndIsCompletedFalse(Long memberId);
 }
