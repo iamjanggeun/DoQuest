424198d0459bb6feae81e0ffdd783d9841138390
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-08-07 00:59:12 +0900

    fix:상속 extends 안했던거 수정

--- DoQuest-server/src/main/java/com/doquest/domain/memo/repository/MemoRepository.java
+++ DoQuest-server/src/main/java/com/doquest/domain/memo/repository/MemoRepository.java
@@ -2,10 +2,10 @@ package com.doquest.domain.memo.repository;
 
 import com.doquest.domain.member.entity.Member;
 import com.doquest.domain.memo.entity.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface MemoRepository {
-
+public interface MemoRepository extends JpaRepository<Memo, Long> {
     List<Memo> findByMemberIdOrderByCreatedAtDesc(Long memberId);
 }
