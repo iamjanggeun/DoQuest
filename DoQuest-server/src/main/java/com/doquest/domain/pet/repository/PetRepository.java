330140dc015e4430806dea9e2ef461e6b9dd5075
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-08-07 01:17:17 +0900

    fix:제네릭 타입 오류 수정

--- DoQuest-server/src/main/java/com/doquest/domain/pet/repository/PetRepository.java
+++ DoQuest-server/src/main/java/com/doquest/domain/pet/repository/PetRepository.java
@@ -1,13 +1,12 @@
 package com.doquest.domain.pet.repository;
 
-import com.doquest.domain.member.entity.Member;
 import com.doquest.domain.pet.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
-    Optional<Member> findByMemberId(Long memberId);
+    Optional<Pet> findByMemberId(Long memberId);
 }
 
 
