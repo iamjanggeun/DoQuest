b8118682c01f5efcaab5e87f1b56fc5d1ec5631a
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-08-06 22:44:18 +0900

    feat:ErrorResponse 추가

---
+++ DoQuest-server/src/main/java/com/doquest/global/error/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.doquest.global.error;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+
+    private final String code;
+    private final String message;
+
+    private ErrorResponse(ErrorCode errorCode) {
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(errorCode);
+    }
+}
