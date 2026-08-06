0f9d3778e767e28f7b27ded7fffa4e6b629bb7d2
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-08-07 00:58:12 +0900

    fix:상속 extends 안했던거 수정

--- DoQuest-server/src/main/java/com/doquest/domain/pet/repository/PetRepository.java
+++ DoQuest-server/src/main/java/com/doquest/domain/pet/repository/PetRepository.java
@@ -1,10 +1,12 @@
 package com.doquest.domain.pet.repository;
 
 import com.doquest.domain.member.entity.Member;
+import com.doquest.domain.pet.entity.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PetRepository {
+public interface PetRepository extends JpaRepository<Pet, Long> {
     Optional<Member> findByMemberId(Long memberId);
 }
 
