47164bb4290d1d1a665b1b23855e0893414a6b76
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-08-06 17:28:43 +0900

    feat:MemberRepository 인터페이스 추가

---
+++ DoQuest-server/src/main/java/com/doquest/domain/member/repository/MemberRepository.java
@@ -0,0 +1,11 @@
+package com.doquest.domain.member.repository;
+
+import com.doquest.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    boolean existByEmail(String email);
+}
