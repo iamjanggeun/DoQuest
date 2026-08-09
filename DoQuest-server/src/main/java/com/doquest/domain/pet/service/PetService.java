3073edd297c8139d37a6a4857d7da98c87b21049
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-08-09 18:34:50 +0900

    feat:PetService 추가

---
+++ DoQuest-server/src/main/java/com/doquest/domain/pet/service/PetService.java
@@ -0,0 +1,25 @@
+package com.doquest.domain.pet.service;
+
+import com.doquest.domain.pet.entity.Pet;
+import com.doquest.domain.pet.repository.PetRepository;
+import com.doquest.global.error.BusinessException;
+import com.doquest.global.error.ErrorCode;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PetService {
+
+    private final PetRepository petRepository;
+
+    /*
+    * 회원 아이디로 펫 단건 조회 (대시보드 서빙용)
+    * */
+    public Pet getPetByMemberId(Long memberId) {
+        return petRepository.findByMemberId(memberId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.PET_NOT_FOUND));
+    }
+}
