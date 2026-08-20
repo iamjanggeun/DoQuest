c7d684351529d4e9777432f1584ac177b1e211ba
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-08-20 16:47:28 +0900

    fix: PetRepository Member->Pet 단방향 참조 조회 JPQL 추가

--- DoQuest-server/src/main/java/com/doquest/domain/pet/repository/PetRepository.java
+++ DoQuest-server/src/main/java/com/doquest/domain/pet/repository/PetRepository.java
@@ -2,11 +2,19 @@ package com.doquest.domain.pet.repository;
 
 import com.doquest.domain.pet.entity.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
-    Optional<Pet> findByMemberId(Long memberId);
+
+    /**
+     * Member가 Pet의 FK(pet_id)를 갖는 단방향 연관관계이므로
+     * Member와 Join하여 해당 회원의 펫을 조회
+     */
+    @Query("SELECT m.pet FROM Member m WHERE m.id = :memberId")
+    Optional<Pet> findByMemberId(@Param("memberId") Long memberId);
 }
 
 
