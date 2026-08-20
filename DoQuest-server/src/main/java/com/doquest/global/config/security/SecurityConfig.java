e94f6ae195428e86a995c845e7fe40b842386c6d
iamjanggeun <120302612+iamjanggeun@users.noreply.github.com>
2026-08-20 16:57:36 +0900

    fix: 요청별 인가 규칙 설정

--- DoQuest-server/src/main/java/com/doquest/global/config/security/SecurityConfig.java
+++ DoQuest-server/src/main/java/com/doquest/global/config/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.doquest.global.config.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -27,6 +29,11 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
 
+                // H2 콘솔의 다중 iframe 렌더링 허용
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+
                 // 세션 사용 안 함 (JWT 기반 무상태성 유지)
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -34,7 +41,10 @@ public class SecurityConfig {
 
                 // 요청별 인가(Authorization) 규칙 설정
                 .authorizeHttpRequests(auth -> auth
-                        // 인증 없이 허용할 화이트리스트 URL (회원가입, 로그인, Swagger 등)
+                        // H2 콘솔 정적 리소스 및 서블릿 경로 허용
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        // 인증 없이 허용할 화이트리스트 URL (회원가입, 로그인 등)
                         .requestMatchers("/api/v1/auth/**").permitAll()
                         // 그 외 모든 API 요청은 JWT 인증 필요
                         .anyRequest().authenticated()
